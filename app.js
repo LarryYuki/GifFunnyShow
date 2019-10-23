bded4bdc38fba89d355a9b066127558752af017e
Larry-nan <lawrence,yinan@gmail.com>
2019-10-23 11:45:13 -0700

    added working code for ajax and append gif

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
-var topices = ['funny animals', 'funny baby', 'funny movies'];
+$(document).ready(function () {
 
-let api_key = 'CB3u9m9EcYrw3EnygcS0FX4P91gsyN8N';
-// let api_key = 'CpiVtI10MbfNYbPPYsXddJqPI4hlSssd';
-let queryUrl = 'https://api.giphy.com/v1/gifs';
+    var topices = ['funny animals', 'funny baby', 'funny movies'];
 
-$('#searchSubmit').on('click', function () {
-    const searchText = $('#gifSearch').val();
-    const queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' +
-        api_key +
-        '&q=' +
-        searchText +
-        '&limit=25&offset=0&rating=G&lang=en';
-    console.log(queryUrl);
-})
-$.ajax({
-    method: 'GET',
-    url: queryUrl
-}).then(response => {
-    const data = response.data;
-    console.log(data);
+    let api_key = 'CB3u9m9EcYrw3EnygcS0FX4P91gsyN8N';
+    // let api_key = 'CpiVtI10MbfNYbPPYsXddJqPI4hlSssd';
+    let queryUrl = 'https://api.giphy.com/v1/gifs';
+    renderButtons();
 
-    data.forEach(element => {
-        console.log(element.images.ficed_height.url);
+    $(document).on('click', '.gif', function () { //listener for dynamicaly generated elements
+        console.log("hello")
 
-        $('.gifContainer').append(`<img src='${elment.images.fixed_height.url}">`);
+        const searchText = ($(this).attr("data-name"))
+        const queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' +
+            api_key +
+            '&q=' +
+            searchText +
+            '&limit=10&offset=0&rating=G&lang=en';
+        console.log(queryUrl);
+        $.ajax({
+            method: 'GET',
+            url: queryUrl
+        }).then(response => {
+            const data = response.data;
+            console.log("nick", data);
 
+            data.forEach(element => {
+                console.log(element.images.fixed_height.url);
+
+                $('.gifContainer').append(`<div></div><img src='${element.images.fixed_height.url}'><p>${element.rating}</p></div>`);
+
+            });
+
+        })
+    })
+
+    function renderButtons() {
+        $("#gif-view").empty();
+        for (var i = 0; i < topices.length; i++) {
+
+            var a = $("<button>");
+            a.addClass("gif");
+            a.attr("data-name", topices[i]);
+            a.text(topices[i]);
+            $("#gif-view").append(a);
+        }
+    }
+    $("#add-gif").on("click", function (e) {
+        e.preventDefault();
+        var topice = $("#gifSearch").val().trim();
+        topices.push(topice);
+        renderButtons();
     });
-})
+});
